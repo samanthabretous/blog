2e56176aad275838b7e0e3ac38926f0d8247a573
samanthabretous <samanthabretous@Samanthas-MacBook-Pro.local>
2020-07-15 07:53:19 -0400

    add readtime to text area

--- src/pages/textarea/Textarea.js
+++ src/pages/textarea/Textarea.js
@@ -4,6 +4,8 @@ import stylesheet from "./index.module.less";
 
 export default function Textarea() {
   const [copySuccess, setCopySuccess] = useState("");
+  const [textAreaValue, setTextAreaValue] = useState("");
+  const [readTime, setReadTime] = useState("");
   const textAreaRef = useRef(null);
   function copyToClipboard(e) {
     textAreaRef.current.select();
@@ -12,33 +14,52 @@ export default function Textarea() {
     setCopySuccess("Copied!");
   }
 
+  function calcReadTime(text) {
+    const wordsPerMinute = 200; // Average case.
+    let result;
+
+    let wordCount = text.split(/\s/g).length; // Split by words
+    if (wordCount > 0) {
+      result = Math.ceil(wordCount / wordsPerMinute);
+    }
+    setReadTime(result - 1);
+  }
+
+  function handleOnChange(event) {
+    calcReadTime(event.target.value);
+    setTextAreaValue(event.target.textAreaRef);
+  }
+
   useEffect(() => {
     setTimeout(() => setCopySuccess(""), 3000);
   }, [copySuccess]);
   return (
-    <form>
-      <textarea
-        ref={textAreaRef}
-        className={stylesheet.textarea}
-        rows="15"
-        cols="75"
-        type="text"
-      />
-      <div className={stylesheet.buttonContainer}>
-        <input
-          type="button"
-          value="Copy"
-          className={stylesheet.button}
-          onClick={copyToClipboard}
+    <div>
+      <form>
+        <textarea
+          ref={textAreaRef}
+          className={stylesheet.textarea}
+          rows="15"
+          cols="75"
+          type="text"
+          onChange={handleOnChange}
         />
-        <input
-          type="reset"
-          defaultValue="Reset"
-          className={stylesheet.button}
-        />
-        <p className={stylesheet.copySucess}>{copySuccess}</p>
-      </div>
-      )
-    </form>
+        <div className={stylesheet.buttonContainer}>
+          <input
+            type="button"
+            value="Copy"
+            className={stylesheet.button}
+            onClick={copyToClipboard}
+          />
+          <input
+            type="reset"
+            defaultValue="Reset"
+            className={stylesheet.button}
+          />
+          <p className={stylesheet.copySucess}>{copySuccess}</p>
+        </div>
+      </form>
+      <p className={stylesheet.readTime}>Read time: ~{readTime} mins</p>
+    </div>
   );
 }
